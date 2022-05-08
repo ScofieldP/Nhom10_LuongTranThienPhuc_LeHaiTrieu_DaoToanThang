dc9154e36d38835142ce332be8ef93d859461d40
ScofieldP <thienphuc.luongtran@gmail.com>
2022-05-08 18:04:17 +0700

    99%

---
+++ app/src/main/java/com/example/nhom10_luongtranthienphuc_lehaitrieu_daotoanthang/DetailFriendActivity.java
@@ -0,0 +1,13 @@
+package com.example.nhom10_luongtranthienphuc_lehaitrieu_daotoanthang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_friend);
+    }
+}
